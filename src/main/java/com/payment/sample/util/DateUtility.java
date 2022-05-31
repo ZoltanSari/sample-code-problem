af8a1ec2f64f9a9aae75fb44b2ccd46c14e06ab4
ZoltanSari <Kingshapy@gmail.com>
2022-05-31 20:36:07 +0200

    add nullCheck to DateUtility

--- src/main/java/com/payment/sample/util/DateUtility.java
+++ src/main/java/com/payment/sample/util/DateUtility.java
@@ -6,10 +6,10 @@ import java.time.format.DateTimeFormatter;
 public final class DateUtility {
 
     public static LocalDate parseStringToLocalDate(String date) {
-        return LocalDate.parse(date, DateTimeFormatter.ofPattern("dd/MM/yy"));
+        return date != null && !date.isEmpty() ? LocalDate.parse(date, DateTimeFormatter.ofPattern("dd/MM/yy")) : null;
     }
 
     public static String formatLocalDateToString(LocalDate date) {
-        return date.format(DateTimeFormatter.ofPattern("dd/MM/yy"));
+        return date != null ? date.format(DateTimeFormatter.ofPattern("dd/MM/yy")) : null;
     }
 }
